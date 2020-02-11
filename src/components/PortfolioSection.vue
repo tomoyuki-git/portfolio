7394243b1080d2eb0eb7d4bc308a0202392cd0c4
pront <tomoyuki153@gmail.com>
2020-02-11 13:21:21 +0900

    PortfolioSection.vueの作成

---
+++ src/components/PortfolioSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center">
+    <h1><slot></slot></h1>
+    <v-card v-for="(item, index) in sectionItems" :key="index" class="ma-2">
+      <v-card-title class="justify-center">
+        {{ item.title }}
+      </v-card-title>
+      <v-card-text>
+        {{ item.text }}
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sectionItems: {
+      type: Array,
+      required: true
+    }
+  },
+}
+</script>

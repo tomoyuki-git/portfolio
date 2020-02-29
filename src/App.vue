ce13cf6852b163342089ea63129a7f8d425fc03e
pront <tomoyuki153@gmail.com>
2020-02-29 14:27:50 +0900

    ヘッダーの設定

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link to="/skill">Skill</router-link>
-      <router-link to="/contact">Contact</router-link>
+    <div class="header-outer">
+      <header>
+        <h1>
+          <router-link to="/">My Portfolio</router-link> 
+        </h1>
+      </header>
+    </div>
+    <div id="gnav" class="header-nav">
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+          <router-link to="/skill">Skill</router-link>
+          <router-link to="/contact">Contact</router-link>
+        </li>
+      </ul>
     </div>
     <router-view/>
+    <footer>
+      <div class="copyright-box">©️ 2019 My Portfolio</div>
+    </footer>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>

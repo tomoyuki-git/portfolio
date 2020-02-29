734a2f6f5c279881110a7ed67c4ff709a8af734a
pront <tomoyuki153@gmail.com>
2020-02-29 13:38:49 +0900

    main.jsにbootstrapのインポートを追加

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue';
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   vuetify,
